4723135d41ea2f033194ed208e4a586b2f64dce7
bulbul <bulbulzetra93@gmail.com>
2023-07-06 19:09:37 +0600

    deployment

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const verifyJWT = (req, res, next) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@believer.igrxpib.mongodb.net/?retryWrites=true&w=majority`;
 
-// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.16zdfjg.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
